093645a2f89e67250f5d40aab3e8f2bf26d0b8c9
tim.cherniavski <tim.cherniavski@ingenious.build>
2021-10-31 21:57:26 +0300

    [infra] Fix fluentd config to pass tag into workers instead of log message.

--- docker/fluentd/worker/minecraft_chat.sh
+++ docker/fluentd/worker/minecraft_chat.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Process chat message fluentd tag (Eg: row.minecraft.telegram.<{username}>.[{time}].miners!{message}
-# Example: row.minecraft.telegram.<masha>.[05:36:01].miners! Custom message in game chat!!.
+# Example: row.minecraft.telegram.<varloc2000>.[18:07:23].miners! Починил сообщения. Можно отправлять из чата игры используя miners! вначале сообщения
 
 while IFS='' read -r line
 do
   time="${line%%].*}" # cut longest match of ].* from the right
-  time="${time#*.[}" # cut shortest match of .[* from the left
+  time="${time##*.[}" # cut longest match of .[* from the left
 
   username="${line%%>.[*}" # cut longest match of .[* from the right
   username="${username#*.<}" # cut shortest match of *. from the left
