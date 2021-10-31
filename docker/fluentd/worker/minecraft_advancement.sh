ca4c2fbc8fd045736da503acd9bacdee14c2ec59
tim.cherniavski <tim.cherniavski@ingenious.build>
2021-10-31 20:54:52 +0300

    [infra] Fix fluentd worker for chat message & advancement tags.

--- docker/fluentd/worker/minecraft_advancement.sh
+++ docker/fluentd/worker/minecraft_advancement.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-# Process advancement fluentd tag (Eg: row.minecraft.advancement.{username}.[{advanceent}]
-# Example: row.minecraft.advancement.masha.[Free the End]
+# Process advancement fluentd tag (Eg: row.minecraft.advancement.<{username}>.[{advanceent}]
+# Example: row.minecraft.advancement.<masha>.[Free the End]
 
 while IFS='' read -r line
 do
-  username="${line%.[*}" # cut shortest match of .[* from the right
-  username="${username#*.}" # cut shortest match of *. from the left
-  username="${username#*.}" # cut shortest match of *. from the left
-  username="${username#*.}" # cut shortest match of *. from the left
+  username="${line%>.[*}" # cut shortest match of .[* from the right
+  username="${username#*.<}" # cut shortest match of *. from the left
 
-  advancement="${line##*[}" # cut longest match of *[ from the left
+  advancement="${line#*.[}" # cut shortest match of *[ from the left
   advancement="${advancement%]}" # cut shortest match of ] from the right
 
   message="$username получил достижение '$advancement'"
--- docker/fluentd/worker/minecraft_chat.sh
+++ docker/fluentd/worker/minecraft_chat.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-# Process chat message fluentd tag (Eg: row.minecraft.telegram.{username}.[{time}].miners!{message}
-# Example: row.minecraft.telegram.masha.[05:36:01].miners! Custom message in game chat!!.
+# Process chat message fluentd tag (Eg: row.minecraft.telegram.<{username}>.[{time}].miners!{message}
+# Example: row.minecraft.telegram.<masha>.[05:36:01].miners! Custom message in game chat!!.
 
 while IFS='' read -r line
 do
   time="${line%%].*}" # cut longest match of ].* from the right
   time="${time##*.[}" # cut longest match of .[* from the left
 
-  username="${line%%.[*}" # cut longest match of .[* from the right
-  username="${username#*.}" # cut shortest match of *. from the left
-  username="${username#*.}" # cut shortest match of *. from the left
-  username="${username#*.}" # cut shortest match of *. from the left
+  username="${line%%>.[*}" # cut longest match of .[* from the right
+  username="${username#*.<}" # cut shortest match of *. from the left
 
   chat_message="${line#*].}" # cut shortest match of *]. from the left
 
